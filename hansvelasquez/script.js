26a8c15a41d728e7a7999d3503c8601b332895d7
GodBlessedHans <haseveco@gmail.com>
2025-11-12 14:12:06 -0500

    Finished exercise #2

--- hansvelasquez/script.js
+++ hansvelasquez/script.js
@@ -1,6 +1,14 @@
 // Ejercicio 1: Lista del Súpermercado
 let compras = ["Manzanas"];
-compras.push("Leche", "Pan")
-compras.unshift("Huevos")
-compras.splice(1,1)
-console.log(compras)
+compras.push("Leche", "Pan");
+compras.unshift("Huevos");
+compras.splice(1,1);
+console.log(compras);
+
+//Ejercicio 2: Pila de Tareas Pendientes (LIFO)
+let tareas = ["Revisar emails"];
+tareas.unshift("Llamar al cliente");
+tareas.unshift("Preparar reporte");
+tareas.splice(0,1);
+tareas.splice(0,1);//Al eliminar la anterior tarea, la siguiente ocupa la posicion 0
+console.log(tareas)
